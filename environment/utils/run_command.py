8fc971e0eb98896b72c3c1be7500f5ac3d4a9881
Arkady Arkhangorodsky <aarkhangorodsky@gmail.com>
2018-02-10 11:54:55 +0530

    Added vimgolf vimrc

--- environment/utils/run_command.py
+++ environment/utils/run_command.py
@@ -1,9 +1,15 @@
+import shutil
 import subprocess
 import os
 
 def run_command(command, edit_file):
-    subprocess.call('vim -c "let @a=\'{}\'" -c \"normal! @a\" -c \"pyfile current_state.py" {} -c \"normal! \x1bZZ\"'.format(command, edit_file), shell=True)
+    os.rename(os.path.expanduser('~/.vimrc'), 'temp.vimrc')
+    shutil.copyfile('environment/vimgolf.vimrc', os.path.expanduser('~/.vimrc'))
+    with open(os.path.expanduser('~/.vim_runtime/learnvim.vim'), 'w+') as f:
+        f.write("let @a='{}'".format(command))
+    subprocess.call('vim -c \"normal! @a\" -c \"pyfile environment/utils/current_state.py" {} -c \"normal! \x1bZZ\"'.format(edit_file), shell=True)
+    os.rename('temp.vimrc', os.path.expanduser('~/.vimrc'))
 
 if __name__ == '__main__':
-    for i in range(1):
-        run_command('iabc\x1bV', os.path.expanduser('test.txt'))
+    for i in range(5):
+        run_command('iabc\x1bV', 'test.txt')
